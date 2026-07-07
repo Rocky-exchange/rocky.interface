e7dbd03cb2c2a30219b981702713c96a5028b491
bek <>
2026-07-07 23:27:30 +0800

    Limit mobile menu to trade route

--- src/modules/lighter/mobile/shared/MobileSlideMenu.tsx
+++ src/modules/lighter/mobile/shared/MobileSlideMenu.tsx
@@ -1,7 +1,8 @@
 // src/modules/lighter/mobile/shared/MobileSlideMenu.tsx
+import { Trans, t } from "@lingui/macro";
 import type { ReactNode } from "react";
 import { Link } from "react-router-dom";
-import { Trans, t } from "@lingui/macro";
+
 import { BottomSheet } from "./BottomSheet";
 import styles from "./MobileSlideMenu.module.scss";
 
@@ -12,13 +13,6 @@ type Props = {
 
 const LINKS: { to: string; label: ReactNode }[] = [
   { to: "/trade", label: <Trans>Trade</Trans> },
-  { to: "/earn", label: <Trans>Earn</Trans> },
-  { to: "/accounts", label: <Trans>Portfolio</Trans> },
-  { to: "/leaderboard", label: <Trans>Leaderboard</Trans> },
-  { to: "/referrals", label: <Trans>Referrals</Trans> },
-  { to: "/points", label: <Trans>Points</Trans> },
-  { to: "/fee-vip", label: <Trans>Fee &amp; VIP</Trans> },
-  { to: "/blog", label: <Trans>Blog</Trans> },
 ];
 
 export function MobileSlideMenu({ open, onOpenChange }: Props) {
